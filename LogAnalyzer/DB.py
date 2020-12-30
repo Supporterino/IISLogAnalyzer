b13f8aa610d6977a3e8949dd64273585abd79c68
Supporterino <lars@roth-kl.de>
2020-12-30 22:51:35 +0100

    new orm ?

--- LogAnalyzer/DB.py
+++ LogAnalyzer/DB.py
@@ -1,7 +1,1 @@
-import sqlalchemy as db
-
-class database:
-    def __init__(self):
-        engine: db._engine.Engine = db.create_engine('sqlite:///logs.db', echo=True)
-        connection = engine.connect()
-        metadata = db.MetaData()
+from storm
--- setup.py
+++ setup.py
@@ -15,11 +15,11 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "License :: OSI Approved :: GNU General Public License v3",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
     install_requires=[
-        'sqlalchemy'
+        'storm'
     ],
 )
